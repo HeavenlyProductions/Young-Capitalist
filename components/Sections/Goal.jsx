651330d1a1ced99a7c1707c4e24de80ff1faf06c
unknown <emmanueljrplays@gmail.com>
2026-05-20 17:36:07 +0000

    Goal section updated

--- components/Sections/Goal.jsx
+++ components/Sections/Goal.jsx
@@ -33,7 +33,8 @@ const Goal = () => {
               </span>
               <input
                 type="number"
-                className="w-3xs mt-1 px-3.5 py-2 border-2 border-cyan-500 rounded-md outline-0"
+                className="w-3xs mt-1 px-3.5 py-2 text-cyan-500 border-2 border-cyan-400 
+                rounded-md outline-0"
                 min={0}
                 defaultValue={0}
               />
@@ -86,19 +87,34 @@ const Goal = () => {
         <div className="grid-six">
           QUICK PRESETS
           <div className="w-208.5 mt-2 flex flex-wrap gap-2">
-            <button className="px-3.5 py-1.5 text-xs font-normal border border-gray-300 rounded-md light-text">
+            <button
+              className="px-3.5 py-1.5 text-xs font-normal border border-gray-300
+             rounded-md light-text"
+            >
               Emergency Fund · ₵5,000
             </button>
-            <button className="px-3.5 py-1.5 text-xs font-normal border border-gray-300 rounded-md light-text">
+            <button
+              className="px-3.5 py-1.5 text-xs font-normal border border-gray-300
+             rounded-md light-text"
+            >
               New Laptop · ₵3,000
             </button>
-            <button className="px-3.5 py-1.5 text-xs font-normal border border-gray-300 rounded-md light-text">
+            <button
+              className="px-3.5 py-1.5 text-xs font-normal border border-gray-300
+             rounded-md light-text"
+            >
               Vacation · ₵8,000
             </button>
-            <button className="px-3.5 py-1.5 text-xs font-normal border border-gray-300 rounded-md light-text">
+            <button
+              className="px-3.5 py-1.5 text-xs font-normal border border-gray-300
+             rounded-md light-text"
+            >
               Down Payment· ₵20,000
             </button>
-            <button className="px-3.5 py-1.5 text-xs font-normal border border-gray-300 rounded-md light-text">
+            <button
+              className="px-3.5 py-1.5 text-xs font-normal border border-gray-300
+             rounded-md light-text"
+            >
               Business Start · ₵15,000
             </button>
           </div>
@@ -106,21 +122,44 @@ const Goal = () => {
       </div>
 
       <div className="border border-gray-300 rounded-md p-7 mb-6">
-        <div>
-          <h1>EMERGENCY FUND</h1>
-          <h1>48%</h1>
+        <div className="flex justify-between items-center font-black leading-0">
+          <h1 className="text-xl">Emergency Fund</h1>
+          <h1 className="text-[28px]">48%</h1>
         </div>
-        <div>
+        <div className="flex justify-between items-start mb-7 text-[10px] tracking-wider">
           <p>Target: ₵5,000 · Over 1 year</p>
           <p>of goal reached</p>
         </div>
-        <div className="w-full p-2 rounded-xl bg-blue-500"></div>
-        <div>
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
+        <div className="w-full p-2 rounded-xl bg-cyan-400"></div>
+        <div className="flex justify-between grow mt-5 gap-5">
+          <div className="w-full flex flex-col gap-1 p-3.5 bg-gray-100 rounded-md">
+            <span className="text-[10px] tracking-widest light-text">
+              SAVINGS IN 1 YR
+            </span>
+            <span className="text-xl font-black">₵2,400</span>
+          </div>
+          <div className="w-full flex flex-col gap-1 p-3.5 bg-gray-100 rounded-md">
+            <span className="text-[10px] tracking-widest light-text">
+              REMAINING
+            </span>
+            <span className="text-xl font-black">₵2,600</span>
+          </div>
+          <div className="w-full flex flex-col gap-1 p-3.5 bg-gray-100 rounded-md">
+            <span className="text-[10px] tracking-widest light-text">
+              TIMELINE TO GOAL
+            </span>
+            <span className="text-xl font-black">2.1 yrs</span>
+          </div>
         </div>
-        <div className="p-2 bg-amber-600"></div>
+        <div
+          className="mt-5 px-5 py-3 text-xs text-[rgb(230,168,23)] tracking-widest 
+          bg-[hsl(0,100%,97%)] border border-[rgb(230,168,23)] rounded-md"
+        >
+          <span>
+            ⚡ To hit your goal in 12 months, save ₵417/mo. Currently saving
+            ₵200/mo
+          </span>
+        </div>
       </div>
 
       <div className="border border-gray-300 rounded-md p-7 mb-6">
